b9f729886ccee3cc9da8329ecec89fda2d0d371c
GongYuhua <gongyuhua@gmail.com>
2018-11-30 12:10:06 +0800

    update api

--- Video-Call-with-Chat/OpenDuo-iOS-Objective-C/OpenDuo/AgoraSigKit/AgoraSigKit.m
+++ Video-Call-with-Chat/OpenDuo-iOS-Objective-C/OpenDuo/AgoraSigKit/AgoraSigKit.m
@@ -29,9 +29,13 @@ NSString *SIG_CHANNEL_INVITE_USER2 = @"SIG_CHANNEL_INVITE_USER2";
     __weak typeof(self) weakSelf = self;
     [self.rtmKit loginByToken:token user:account completion:^(AgoraRtmLoginErrorCode errorCode) {
         if (errorCode == AgoraRtmLoginErrorOk) {
-            weakSelf.onLoginSuccess(account);
+            if (weakSelf.onLoginSuccess) {
+                weakSelf.onLoginSuccess(account);
+            }
         } else {
-            weakSelf.onLoginFailed(errorCode);
+            if (weakSelf.onLoginFailed) {
+                weakSelf.onLoginFailed(errorCode);
+            }
         }
     }];
 }
@@ -39,27 +43,35 @@ NSString *SIG_CHANNEL_INVITE_USER2 = @"SIG_CHANNEL_INVITE_USER2";
 - (void)logout {
     __weak typeof(self) weakSelf = self;
     [self.rtmKit logoutWithCompletion:^(AgoraRtmLogoutErrorCode errorCode) {
-        weakSelf.onLogout(errorCode);
+        if (weakSelf.onLogout) {
+            weakSelf.onLogout(errorCode);
+        }
     }];
 }
 
 - (void)queryUserStatus:(NSString *)account {
     AgoraRtmMessage *message = [[AgoraRtmMessage alloc] initWithText:SIG_QUERY_USER_STATUS];
     __weak typeof(self) weakSelf = self;
-    [self.rtmKit sendMessageToPeer:account message:message completion:^(AgoraRtmSendPeerMessageState state) {
+    [self.rtmKit sendMessage:message toPeer:account completion:^(AgoraRtmSendPeerMessageState state) {
         NSString* status = (state == AgoraRtmSendPeerMessageStateReceivedByPeer ? @"1" : @"0");
-        weakSelf.onQueryUserStatusResult(account, status);
+        if (weakSelf.onQueryUserStatusResult) {
+            weakSelf.onQueryUserStatusResult(account, status);
+        }
     }];
 }
 
 - (void)channelInviteUser:(NSString *)channelID account:(NSString *)account uid:(uint32_t)uid {
     AgoraRtmMessage *message = [[AgoraRtmMessage alloc] initWithText:SIG_CHANNEL_INVITE_USER];
     __weak typeof(self) weakSelf = self;
-    [self.rtmKit sendMessageToPeer:account message:message completion:^(AgoraRtmSendPeerMessageState state) {
+    [self.rtmKit sendMessage:message toPeer:account completion:^(AgoraRtmSendPeerMessageState state) {
         if (state == AgoraRtmSendPeerMessageStateReceivedByPeer) {
-            weakSelf.onInviteReceivedByPeer(channelID, account, uid);
+            if (weakSelf.onInviteReceivedByPeer) {
+                weakSelf.onInviteReceivedByPeer(channelID, account, uid);
+            }
         } else {
-            weakSelf.onInviteFailed(channelID, account, 0, state, nil);
+            if (weakSelf.onInviteFailed) {
+                weakSelf.onInviteFailed(channelID, account, 0, state, nil);
+            }
         }
     }];
 }
@@ -67,40 +79,50 @@ NSString *SIG_CHANNEL_INVITE_USER2 = @"SIG_CHANNEL_INVITE_USER2";
 - (void)channelInviteUser2:(NSString *)channelID account:(NSString *)account extra:(NSString *)extra {
     AgoraRtmMessage *message = [[AgoraRtmMessage alloc] initWithText:SIG_CHANNEL_INVITE_USER2];
     __weak typeof(self) weakSelf = self;
-    [self.rtmKit sendMessageToPeer:account message:message completion:^(AgoraRtmSendPeerMessageState state) {
+    [self.rtmKit sendMessage:message toPeer:account completion:^(AgoraRtmSendPeerMessageState state) {
         if (state == AgoraRtmSendPeerMessageStateReceivedByPeer) {
-            weakSelf.onInviteReceivedByPeer(channelID, account, 0);
+            if (weakSelf.onInviteReceivedByPeer) {
+                weakSelf.onInviteReceivedByPeer(channelID, account, 0);
+            }
         } else {
-            weakSelf.onInviteFailed(channelID, account, 0, state, nil);
+            if (weakSelf.onInviteFailed) {
+                weakSelf.onInviteFailed(channelID, account, 0, state, nil);
+            }
         }
     }];
 }
 
 - (void)channelInviteAccept:(NSString *)channelID account:(NSString *)account uid:(uint32_t)uid extra:(NSString *)extra {
     AgoraRtmMessage *message = [[AgoraRtmMessage alloc] initWithText:SIG_CHANNEL_INVITE_ACCEPT];
-    [self.rtmKit sendMessageToPeer:account message:message completion:nil];
+    [self.rtmKit sendMessage:message toPeer:account completion:nil];
 }
 
 - (void)channelInviteRefuse:(NSString *)channelID account:(NSString *)account uid:(uint32_t)uid extra:(NSString *)extra {
     AgoraRtmMessage *message = [[AgoraRtmMessage alloc] initWithText:SIG_CHANNEL_INVITE_REFUSE];
-    [self.rtmKit sendMessageToPeer:account message:message completion:nil];
+    [self.rtmKit sendMessage:message toPeer:account completion:nil];
 }
 
 - (void)channelInviteEnd:(NSString *)channelID account:(NSString *)account uid:(uint32_t)uid {
     AgoraRtmMessage *message = [[AgoraRtmMessage alloc] initWithText:SIG_CHANNEL_INVITE_END];
     __weak typeof(self) weakSelf = self;
-    [self.rtmKit sendMessageToPeer:account message:message completion:^(AgoraRtmSendPeerMessageState state) {
-        weakSelf.onInviteEndByMyself(channelID, account, uid);
+    [self.rtmKit sendMessage:message toPeer:account completion:^(AgoraRtmSendPeerMessageState state) {
+        if (weakSelf.onInviteEndByMyself) {
+            weakSelf.onInviteEndByMyself(channelID, account, uid);
+        }
     }];
 }
 
 - (void)rtmKit:(AgoraRtmKit *)kit connectionStateChanged:(AgoraRtmConnectionState)state {
     switch (state) {
         case AgoraRtmConnectionStateConnected:
-            self.onReconnected(0);
+            if (self.onReconnected) {
+                self.onReconnected(0);
+            }
             break;
         case AgoraRtmConnectionStateDisConnected:
-            self.onReconnecting(-1);
+            if (self.onReconnecting) {
+                self.onReconnecting(-1);
+            }
         default:
             break;
     }
@@ -119,13 +141,21 @@ NSString *SIG_CHANNEL_INVITE_USER2 = @"SIG_CHANNEL_INVITE_USER2";
 
 - (void)handleSignalString:(NSString *)string fromPeer:(NSString *)peerId {
     if ([string isEqualToString:SIG_CHANNEL_INVITE_USER] || [string isEqualToString:SIG_CHANNEL_INVITE_USER2]) {
-        self.onInviteReceived(nil, peerId, 0, nil);
+        if (self.onInviteReceived) {
+            self.onInviteReceived(nil, peerId, 0, nil);
+        }
     } else if ([string isEqualToString:SIG_CHANNEL_INVITE_ACCEPT]) {
-        self.onInviteAcceptedByPeer(nil, peerId, 0, nil);
+        if (self.onInviteAcceptedByPeer) {
+            self.onInviteAcceptedByPeer(nil, peerId, 0, nil);
+        }
     } else if ([string isEqualToString:SIG_CHANNEL_INVITE_REFUSE]) {
-        self.onInviteRefusedByPeer(nil, peerId, 0, nil);
+        if (self.onInviteRefusedByPeer) {
+            self.onInviteRefusedByPeer(nil, peerId, 0, nil);
+        }
     } else if ([string isEqualToString:SIG_CHANNEL_INVITE_END]) {
-        self.onInviteEndByPeer(nil, peerId, 0, nil);
+        if (self.onInviteEndByPeer) {
+            self.onInviteEndByPeer(nil, peerId, 0, nil);
+        }
     }
 }
 @end
